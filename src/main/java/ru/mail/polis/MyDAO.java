c9ae07be176f4ac8d07de6c2843c136ff273b266
Nadya <spinakernadya7@gmail.com>
2019-04-17 23:14:50 +0300

    def

---
+++ src/main/java/ru/mail/polis/MyDAO.java
@@ -0,0 +1,35 @@
+package ru.mail.polis;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class MyDAO implements DAO{
+
+    private final SortedMap<ByteBuffer, Record> data = new TreeMap<>();
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) {
+        return data.tailMap(from).values().iterator();
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) {
+        data.put(key, Record.of(key, value));
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) {
+        data.remove(key);
+    }
+
+    @Override
+    public void close() throws IOException {
+        // do nothing
+    }
+}
